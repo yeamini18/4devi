4192fe1da6cf4a15f915d471fe4bda02e5ddcdbc
poornima-max <poorniveluit@gmail.com>
2026-04-21 21:00:58 +0530

    sample program

---
+++ src/gitPractice/Arithmatic.java
@@ -0,0 +1,15 @@
+package gitPractice;
+
+public class Arithmatic {
+    int a = 100;
+    int b = 200;
+    public void addNumbers(){
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+        Arithmatic   ac = new Arithmatic();
+        ac.addNumbers();
+    }
+
+}
